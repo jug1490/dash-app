1d8ceb9d05a05dcc9b99771de7676d7406310389
jug1490 <johnag1130@gmail.com>
2024-07-03 10:14:38 -0500

    Update wildfire.py

--- wildfire.py
+++ wildfire.py
@@ -7,6 +7,7 @@ import plotly.express as px
 from dash import no_update
 import datetime as dt
 import os
+import calendar
 #Create app
 app = dash.Dash(__name__)
 # Clear the layout and do not display exception till callback gets executed
@@ -33,24 +34,23 @@ html.Div(children=[
             # First inner divsion for  adding dropdown helper text for Selected Drive wheels
             html.Div([html.H2('Select Region:', style={'margin-right': '2em'}),
             #Radio items to select the region
-            dcc.RadioItems(['NSW','QL','SA','TA','VI','WA'], 'NSW', id='region',inline=True),
+            # dcc.RadioItems(['NSW','QL','SA','TA','VI','WA'], 'NSW', id='region',inline=True),
             dcc.RadioItems([{"label":"New South Wales","value": "NSW"},
                             {"label":"Northern Territory","value": "NT"},
                             {"label":"Queensland","value": "QL"},
                             {"label":"South Australia","value": "SA"},
                             {"label":"Tasmania","value": "TA"},
                             {"label":"Victoria","value": "VI"},
-                            {"label":"Western Australia","value": "WA"}],"NSW", id='region1',inline=True)]),
+                            {"label":"Western Australia","value": "WA"}],"NSW", id='region',inline=True)]),
 #           #Dropdown to select year
             html.Div([
                     html.H2('Select Year:', style={'margin-right': '2em'}),
                     dcc.Dropdown(df.Year.unique(), value = 2005,id='year'),
-                    html.Div([
-                
+            ]),
+            html.Div([
                         html.Div([ ], id='plot1'),
                         html.Div([ ], id='plot2'),
-                    ])
-            ]),
+                    ], style={'display': 'flex'})
         ])
     ])
 # #Second Inner division for adding 2 inner divisions for 2 output graphs
@@ -70,19 +70,25 @@ html.Div(children=[
 #Place to define the callback function .
 def reg_year_display(input_region,input_year):
     
-    #data
-   region_data = df[df['Region'] == input_region]
-   y_r_data = region_data[region_data['Year']==input_year]
+    # Filter by region
+    region_data = df[df['Region'] == input_region]
+
+    # Extract Year and Month
+    region_data['Year'] = list(map(int, map(lambda d: d.split('/')[2], region_data.loc[:, 'Date'])))
+    region_data['Month'] = list(map(int, map(lambda d: d.split('/')[0], region_data.loc[:,'Date'])))
+
+    region_data['Month_Name'] = list(map(lambda i: calendar.month_name[i], region_data['Month']))
+    y_r_data = region_data[region_data['Year']== int(input_year)]
     #Plot one - Monthly Average Estimated Fire Area
-   
-   est_data = y_r_data.groupby('Month')['Estimated_fire_area'].mean().reset_index()
-   fig1 = px.pie(est_data, title="{} : Monthly Average Estimated Fire Area in year {}".format(input_region,input_year))
+
+    est_data = y_r_data.groupby('Month_Name')['Estimated_fire_area'].mean().reset_index()
+    fig1 = px.pie(values=est_data['Estimated_fire_area'], names=est_data['Month_Name'], title="{} : Monthly Average Estimated Fire Area in year {}".format(input_region,input_year))
    
      #Plot two - Monthly Average Count of Pixels for Presumed Vegetation Fires
-   veg_data = y_r_data.groupby('Month')['Count'].mean().reset_index()
-   fig2 = px.bar(veg_data, title='{} : Average Count of Pixels for Presumed Vegetation Fires in year {}'.format(input_region,input_year))
+    veg_data = y_r_data.groupby('Month_Name')['Count'].mean().reset_index()
+    fig2 = px.bar(x=veg_data['Month_Name'], y=veg_data['Count'], title='{} :Average Count of Pixels for Presumed Vegetation Fires in year {}'.format(input_region,input_year))
     
-   return [dcc.Graph(figure=fig1),
-            dcc.Graph(figure=fig2)]
+    return [dcc.Graph(figure=fig1),
+                dcc.Graph(figure=fig2)]
 if __name__ == '__main__':
     app.run_server()
